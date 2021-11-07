3a5dbd9a796e2e7eb7c641a34386b1933d7774d0
Wmbat <wmbat-dev@protonmail.com>
2021-11-07 16:12:38 -0500

    Last minute stuff

--- sequential-qsort/sequential-qsort/sequential_qsort.cpp
+++ sequential-qsort/sequential-qsort/sequential_qsort.cpp
@@ -2,7 +2,19 @@
 #include <functional>
 #include <iostream>
 #include <iterator>
+#include <random>
 #include <vector>
+
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+using f64 = double;
+
+static constexpr i64 random_generation_bound = 1000;
+static constexpr i64 total_elements = 10000;
 
 template <typename It>
 void qsort(It beg, It end)
@@ -23,9 +35,24 @@ void qsort(It beg, It end)
    }
 }
 
+auto generate_random_array(i64 count) -> std::vector<i32>
+{
+   std::random_device rd;
+   auto random_engine = std::default_random_engine(rd());
+   auto distribution = std::uniform_int_distribution<i32>(0, random_generation_bound);
+
+   auto data = std::vector<i32>(count);
+   for (auto& val : data)
+   {
+      val = distribution(random_engine);
+   }
+
+   return data;
+}
+
 auto main() -> int
 {
-   auto test = std::vector<int>({3, 5, 8, 13, 2, 1}); // NOLINT
+   auto test = generate_random_array(total_elements);
 
    qsort(test.begin(), test.end());
 
